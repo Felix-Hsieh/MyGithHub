bfcc75f42b0d03ab1e682310172c767452868e06
Felix-Hsieh <48756173+Felix-Hsieh@users.noreply.github.com>
2019-05-15 16:01:51 +0800

    Update Program.cs

--- test1/Program.cs
+++ test1/Program.cs
@@ -10,7 +10,7 @@ namespace test1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("12ee34");
+            Console.WriteLine("1234");
             Console.Read();
         }
     }
